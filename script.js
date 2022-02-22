22147394b0932b4a49f3efeefcbf5ca3b21dc0eb
Sarah Holloway <61999030+honeysucklehoney@users.noreply.github.com>
2022-02-22 14:46:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+console.log("hi");
+
+var abouttab = document.getElementById('tab2');
+var projecttab = document.getElementById('tab3');
+var eventstab = document.getElementById('tab4');
+var merchtab = document.getElementById('tab5');
+var donatetab = document.getElementById('tab6');
+
+
+
+function openAbout()
+
+{
+  var abouttab = document.getElementById('tab2');
+  var projecttab = document.getElementById('tab3');
+  var eventstab = document.getElementById('tab4');
+  var merchtab = document.getElementById('tab5');
+  var donatetab = document.getElementById('tab6');
+
+
+  abouttab.style.display ="block";
+  projecttab.style.display ="none";
+  eventstab.style.display ="none";
+  merchtab.style.display ="none";
+  donatetab.style.display ="none";
+}
+
+
+function openProjects()
+
+{
+  var abouttab = document.getElementById('tab2');
+  var projecttab = document.getElementById('tab3');
+  var eventstab = document.getElementById('tab4');
+  var merchtab = document.getElementById('tab5');
+  var donatetab = document.getElementById('tab6');
+
+  abouttab.style.display ="none";
+  projecttab.style.display ="block";
+  eventstab.style.display ="none";
+  merchtab.style.display ="none";
+  donatetab.style.display ="none";
+}
+
+function openEvents()
+
+{
+  var abouttab = document.getElementById('tab2');
+  var projecttab = document.getElementById('tab3');
+  var eventstab = document.getElementById('tab4');
+  var merchtab = document.getElementById('tab5');
+  var donatetab = document.getElementById('tab6');
+
+  abouttab.style.display ="none";
+  projecttab.style.display ="none";
+  eventstab.style.display ="block";
+  merchtab.style.display ="none";
+  donatetab.style.display ="none";
+}
+
+function openMerch()
+
+{
+  var abouttab = document.getElementById('tab2');
+  var projecttab = document.getElementById('tab3');
+  var eventstab = document.getElementById('tab4');
+  var merchtab = document.getElementById('tab5');
+  var donatetab = document.getElementById('tab6');
+
+  abouttab.style.display ="none";
+  projecttab.style.display ="none";
+  eventstab.style.display ="none";
+  merchtab.style.display ="block";
+  donatetab.style.display ="none";
+}
+
+function openDonate()
+
+{
+  var abouttab = document.getElementById('tab2');
+  var projecttab = document.getElementById('tab3');
+  var eventstab = document.getElementById('tab4');
+  var merchtab = document.getElementById('tab5');
+  var donatetab = document.getElementById('tab6');
+
+  abouttab.style.display ="none";
+  projecttab.style.display ="none";
+  eventstab.style.display ="none";
+  merchtab.style.display ="none";
+  donatetab.style.display ="block";
+}
